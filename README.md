175246a25a834c42e9afc752f64d83466489f14c
Tim Shaker <tim@tmshkr.com>
2020-04-08 15:14:58 -0700

    list of users

--- README.md
+++ README.md
@@ -95,7 +95,7 @@ We have provided test data for the resources.
 - [x] Implement all endpoints and middleware within `posts/postRouter.js`
 
 - [ ] Create a React App
-  - [ ] Use `create-react-app` to create an application inside the root folder, name it `client`.
-  - [ ] From the React application connect to the `/api/users` endpoint in the API and show the list of users.
+  - [x] Use `create-react-app` to create an application inside the root folder, name it `client`.
+  - [x] From the React application connect to the `/api/users` endpoint in the API and show the list of users.
   - [ ] Add functionality to show the details of a user, including their posts, when clicking a user name in the list. Use React Router to navigate to a `/users/:id` route to show the user details.
   - [ ] Add styling!
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./utils/axios";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./App.scss";
 
+import Users from "./components/Users";
+import UserForm from "./components/UserForm";
+
 function App() {
-  return <main className="App">hello world</main>;
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    return axios
+      .get("/api/users")
+      .then(({ data }) => setUsers(data))
+      .catch((err) => console.dir(err));
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <Router>
+      <main className="App">
+        <Switch>
+          <Route
+            exact
+            path="/users"
+            render={(props) => (
+              <Users {...props} users={users} getUsers={getUsers} />
+            )}
+          />
+          <Route
+            exact
+            path="/users/add"
+            render={(props) => (
+              <UserForm {...props} users={users} getUsers={getUsers} />
+            )}
+          />
+          <Route
+            exact
+            path="/users/edit/:id"
+            render={(props) => (
+              <UserForm {...props} users={users} getUsers={getUsers} />
+            )}
+          />
+          <Redirect to="/users" />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
