52fdf7fac1d5ba5e6515f1a5902ee0e52482bdc3
Tim Shaker <tim@tmshkr.com>
2020-04-08 13:24:48 -0700

    GET /api/posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,37 @@
 const express = require("express");
+const Posts = require("./postDb");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  Posts.get()
+    .then((posts) => res.json(posts))
+    .catch((err) => {
+      console.error(err);
+      next({ code: 500, message: "There was a problem retrieving the posts" });
+    });
+});
+
+router.get("/:id", validatePostId, (req, res) => {
+  res.json(req.post);
+});
+
+router.delete("/:id", validatePostId, (req, res, next) => {
   res.json({ api: "running..." });
 });
 
-router.get("/:id", (req, res, next) => {
-  res.json({ api: "running..." });
-});
-
-router.delete("/:id", (req, res, next) => {
-  res.json({ api: "running..." });
-});
-
-router.put("/:id", (req, res, next) => {
+router.put("/:id", validatePostId, (req, res, next) => {
   res.json({ api: "running..." });
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+async function validatePostId(req, res, next) {
+  const { id } = req.params;
+  const post = await Posts.getById(id);
+  if (!post) return next({ code: 400, message: "invalid post id" });
+  req.post = post;
+  next();
 }
 
 module.exports = router;
